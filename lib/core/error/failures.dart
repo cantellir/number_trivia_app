66814661487ff6121d0970a259d22ba41b67190b
rodrigo <cantelli.r@gmail.com>
2020-02-17 22:45:16 -0300

    created datasource abstract classes

--- lib/core/error/failures.dart
+++ lib/core/error/failures.dart
@@ -6,3 +6,7 @@ abstract class Failure extends Equatable {
   @override
   List<Object> get props => const<dynamic>[];
 }
+
+class ServerFailure extends Failure {}
+
+class CacheFailure extends Failure {}
